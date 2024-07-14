a61f5e92bedc26d3564f3aabda6711ea98d45780
Mehar-Aziz <meharaziz.1614>
2024-07-14 14:08:09 +0500

    banker roulette

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import random
+
 name_string = input("Gve everybody's names, separated by a comma. ")
 names = name_string.split(", ")
-print(names)
+random_index = random.randint(0, len(names)-1)
+
+random_name = names[random_index]
+
+print(f"{random_name} is going to buy today's meal")
